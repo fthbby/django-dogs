41da771c27c166370dbdfa83e6b0f35835971398
Nat <fthbby@gmail.com>
2022-03-26 14:05:54 -0700

    added img for dogtoys

--- main_app/models.py
+++ main_app/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 class DogToy(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=50)
+    img = models.CharField(max_length=250)
+
 
     def __str__(self):
         return self.name
